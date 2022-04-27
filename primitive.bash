f275dd0ab0419ab194f7f14df488ec16eb25ea9d
林博仁(Buo-ren Lin) <Buo.Ren.Lin@gmail.com>
2022-04-28 03:01:28 +0800

    feat: Implement primitive variant
    Signed-off-by: 林博仁(Buo-ren Lin) <Buo.Ren.Lin@gmail.com>

---
+++ primitive.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# _script_description_
+#
+# Copyright _copyright_effective_year_ _copyright_holder_name_ <_copyright_holder_contact_>
+# SPDX-License-Identifier: CC-BY-SA-4.0
+
+set \
+    -o errexit \
+    -o errtrace \
+    -o nounset \
+    -o pipefail
