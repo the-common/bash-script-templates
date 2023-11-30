4652dafd43b64a41df5b7bef95e917b683860228
林博仁(Buo-ren, Lin) <Buo.Ren.Lin@gmail.com>
2023-11-30 11:56:45 +0800

    fix: Improve documentation and robustability of the defensive interpreter behavior setting logic
    Signed-off-by: 林博仁(Buo-ren, Lin) <Buo.Ren.Lin@gmail.com>

--- primitive.bash
+++ primitive.bash
@@ -4,7 +4,18 @@
 # Copyright _copyright_effective_year_ _copyright_holder_name_ <_copyright_holder_contact_>
 # SPDX-License-Identifier: CC-BY-SA-4.0
 
-set \
-    -o errexit \
-    -o errtrace \
+set_opts=(
+    # Terminate script execution when an unhandled error occurs
+    -o errexit
+    -o errtrace
+
+    # Terminate script execution when an unset parameter variable is
+    # referenced
     -o nounset
+)
+if ! set "${set_opts[@]}"; then
+    printf \
+        'Error: Unable to set the defensive interpreter behavior.\n' \
+        1>&2
+    exit 1
+fi
